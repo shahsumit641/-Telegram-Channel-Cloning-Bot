3b25df22ca3f0bdab81af6e05920d240b6133ebc
shahsumit641 <shahsumit641@gmail.com>
2026-04-30 11:12:06 +0545

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Required
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-DATABASE_URL = os.getenv("DATABASE_URL", "")
-SUPABASE_URL = os.getenv("SUPABASE_URL", "")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 # Optional
 AUTO_FORWARD_INTERVAL = int(os.getenv("AUTO_FORWARD_INTERVAL", "10"))
